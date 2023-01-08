b859b5c4b046ecddf9ba5e1f7bc30d6d5636e267
ComradeFreeman <mister.siroejkin@yandex.ru>
2023-01-08 12:06:23 +0200

    Add MiniCore as dependency, remove some unessessary files from git

---
+++ device/firmware/libs/ringbuffer/example.c
@@ -0,0 +1,125 @@
+//#include "libs.h"
+#include "RingBuffer.h"
+#include <stdio.h>
+
+void example_8bit(void);
+void example_16bit(void);
+void example_ovf(void);
+
+int main() {
+    example_8bit();
+    example_16bit();
+    example_ovf();
+    return 0;
+}
+
+void example_8bit(void){
+    static RINGBUF_t ringbuf8; // ringbuffer instance
+    static uint8_t ringbuf8_data[20] = {0,}; // static data buffer
+    RingBuf_Init(ringbuf8_data, 20, sizeof(uint8_t), &ringbuf8);
+    uint16_t avail8 = 0;
+
+    // Input array
+    uint8_t in8[5] = {10, 15, 24, 255, 8};
+
+    // Put 1 byte
+    RingBuf_BytePut(in8[0], &ringbuf8); // {10, ... }
+
+    // Check availability after putting 1 byte
+    RingBuf_Available(&avail8, &ringbuf8); // avail: 1
+    printf("Avail_ %u", (unsigned int)avail8);
+
+    // Put data from specific part of array
+    RingBuf_DataPut(&in8[2], 3, &ringbuf8); // {10, 24, 255, 8, ...}
+
+    // Check availability after putting 3 bytes
+    RingBuf_Available(&avail8, &ringbuf8); // avail: 4
+    printf("Avail_ %u", (unsigned int)avail8);
+
+    uint8_t out8_read = 0; // output value after reading
+    // Read 1 byte
+    RingBuf_ByteRead(&out8_read, &ringbuf8); // 10
+
+    // Check availability after reading 1 byte
+    RingBuf_Available(&avail8, &ringbuf8); // avail: 3
+
+    uint8_t out8_watch = 0; // output value after watching
+    // Watch without flushing
+    RingBuf_ByteWatch(&out8_watch, &ringbuf8); // 24
+
+    // Check availability after watching 1 byte
+    RingBuf_Available(&avail8, &ringbuf8); // avail: 3
+
+    uint8_t out8[5] = {0,}; // output array for reading
+    RingBuf_DataRead(out8, avail8, &ringbuf8); // {24, 255, ...}
+
+    // Check availability after reading 3 bytes
+    RingBuf_Available(&avail8, &ringbuf8); // avail: 0
+
+    asm("nop"); // point for debugging
+}
+
+void example_16bit(void){
+    static RINGBUF_t ringbuf16; // ringbuffer instance
+    static uint16_t ringbuf16_data[10] = {0,}; // static data buffer
+    RingBuf_Init(ringbuf16_data, 10, sizeof(uint16_t), &ringbuf16);
+    uint16_t avail16 = 0;
+
+    // Input array
+    uint16_t in16[5] = {3443, 1004, 1337, 2281, 1234};
+
+    // Put 1 cell (16 bit variable)
+    RingBuf_CellPut((uint16_t*)&in16[0], &ringbuf16); // {3443, ...}
+
+    // Check availability after putting 1 cell
+    RingBuf_Available(&avail16, &ringbuf16); // avail: 1
+
+    // Put data from specific part of array
+    RingBuf_DataPut((uint16_t*)&in16[2], 3, &ringbuf16); // {3443, 1337, 2281, 1234, ...}
+
+    // Check availability after putting 3 cells
+    RingBuf_Available(&avail16, &ringbuf16); // avail: 4
+
+    uint16_t out16_read = 0; // output value after reading
+    // Read 1 cell (16 bit variable)
+    RingBuf_CellRead(&out16_read, &ringbuf16); // 3443
+
+    // Check availability after reading 1 cell
+    RingBuf_Available(&avail16, &ringbuf16); // avail: 3
+
+    uint16_t out16_watch = 0; // output value after watching
+    // Watch without flushing
+    RingBuf_CellWatch(&out16_watch, &ringbuf16); // 1337
+
+    // Check availability after watching 1 cell
+    RingBuf_Available(&avail16, &ringbuf16); // avail: 3
+
+    uint16_t out16[5] = {0,}; // output array for reading
+    RingBuf_DataRead(out16, avail16, &ringbuf16); // {24, 255, ...}
+
+    // Check availability after reading 3 bytes
+    RingBuf_Available(&avail16, &ringbuf16); // avail: 0
+
+    asm("nop"); // point for debugging
+}
+
+void example_ovf(void){
+    static RINGBUF_t ringbuf;
+    static uint32_t data[3] = {0,};
+    RingBuf_Init(data, 3, 4, &ringbuf);
+
+    uint32_t cell = 66890;
+    RingBuf_CellPut(&cell, &ringbuf); // head: 1; tail: 0
+    uint32_t out1 = 0;
+    RingBuf_CellRead(&out1, &ringbuf); // head: 1; tail: 1
+
+    // only 2 cells left in cells' array, let's put 3 cells
+    uint32_t data_in[3] = {727270, 917020, 812734};
+    RingBuf_DataPut(data_in, 3, &ringbuf); // data: {727270, 917020, 812734}
+    uint32_t data_out[3] = {0,};
+    RingBuf_DataRead(data_out, 3, &ringbuf);
+    // data_out: {727270, 917020, 812734}
+
+    // Point for debugging
+    asm("nop");
+}
